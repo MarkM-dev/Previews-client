cb78d2bf115504f99cad081134b81817be522f95
Mark Magilnitzki <markm.dev@gmail.com>
2020-02-25 04:43:05 +0200

    -- added image cache logic (20 secs).

--- core.js
+++ core.js
@@ -1,6 +1,7 @@
 var isNavBarCollapsed;
 var previewDiv = null;
 var appendContainer;
+var IMAGE_CACHE_TTL_MS = 20000;
 
 function getElementOffset(el) {
     var rect = el.getBoundingClientRect(),
@@ -17,16 +18,26 @@ function createAndShowPreview(navCardEl) {
     previewDiv.style.marginTop = (getElementOffset(navCardEl).top + 45) + "px";
     previewDiv.style.marginLeft = isNavBarCollapsed? "6rem":"25rem";
     previewDiv.style.zIndex = "9999999";
-    previewDiv.style.backgroundImage = "url('https://static-cdn.jtvnw.net/previews-ttv/live_user_" + navCardEl.href.substr(navCardEl.href.lastIndexOf("/") + 1) + "-440x248.jpg')";
+    previewDiv.style.backgroundImage = "url('https://static-cdn.jtvnw.net/previews-ttv/live_user_" + navCardEl.href.substr(navCardEl.href.lastIndexOf("/") + 1) + "-440x248.jpg?" + navCardEl.lastImageLoadTimeStamp + "')";
     previewDiv.style.backgroundSize = "cover";
     previewDiv.style.borderRadius = "5px";
     previewDiv.style.boxShadow = "10px 15px 10px -5px rgba(23,23,23,0.75)";
     previewDiv.style.display = "block";
+
+    navCardEl.lastImageLoadTimeStamp = new Date().getTime();
+
     appendContainer.appendChild(previewDiv);
 }
 
 function changeAndShowPreview(navCardEl) {
-    previewDiv.style.backgroundImage = "url('https://static-cdn.jtvnw.net/previews-ttv/live_user_" + navCardEl.href.substr(navCardEl.href.lastIndexOf("/") + 1) + "-440x248.jpg')";
+    if (new Date().getTime() - navCardEl.lastImageLoadTimeStamp > IMAGE_CACHE_TTL_MS) {
+        console.log("refreshing");
+        navCardEl.lastImageLoadTimeStamp = new Date().getTime();
+        previewDiv.style.backgroundImage = "url('https://static-cdn.jtvnw.net/previews-ttv/live_user_" + navCardEl.href.substr(navCardEl.href.lastIndexOf("/") + 1) + "-440x248.jpg?" + navCardEl.lastImageLoadTimeStamp + "')";
+    } else {
+        previewDiv.style.backgroundImage = "url('https://static-cdn.jtvnw.net/previews-ttv/live_user_" + navCardEl.href.substr(navCardEl.href.lastIndexOf("/") + 1) + "-440x248.jpg?" + navCardEl.lastImageLoadTimeStamp + "')";
+    }
+   // previewDiv.style.backgroundImage = "url('https://static-cdn.jtvnw.net/previews-ttv/live_user_" + navCardEl.href.substr(navCardEl.href.lastIndexOf("/") + 1) + "-440x248.jpg?')";
     previewDiv.style.marginTop = (getElementOffset(navCardEl).top + 45) + "px";
     previewDiv.style.marginLeft = isNavBarCollapsed? "6rem":"25rem";
     previewDiv.style.display = "block";
@@ -39,7 +50,7 @@ function hidePreview() {
 
 function setMouseOverListeners(navCardEl) {
     navCardEl.onmouseover = function () {
-        console.log("hover");
+      //  console.log("hover");
         if (previewDiv) {
             changeAndShowPreview(navCardEl);
         } else {
@@ -84,6 +95,7 @@ function refreshNavCardsListAndListeners() {
         navCards = document.getElementsByClassName('side-nav-card__link');
     }
     for (var i = 0; i < navCards.length; i++) {
+        navCards[i].lastImageLoadTimeStamp = new Date().getTime();
         setMouseOverListeners(navCards[i]);
     }
 }
