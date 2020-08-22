044048fc32c261db2bb4f0f7e6f1c8826bedea03
Mark Magilnitzki <markm.dev@gmail.com>
2020-08-22 12:50:39 +0300

    -- update event.

--- main/background.js
+++ main/background.js
@@ -11,18 +11,18 @@ ga('send', 'pageview', 'main');
 var HEART_BEAT_INTERVAL_MS = 325000;
 var lastHeartBeat = new Date().getTime() - HEART_BEAT_INTERVAL_MS;
 
-/*chrome.runtime.onInstalled.addListener(function(details) {
+chrome.runtime.onInstalled.addListener(function(details) {
     if (details.reason == "update")
     {
         chrome.tabs.create({url:"../popups/updatePopup.html"});
         try {
-            ga('send', 'event', 'updatePopup_show-v1.3.6', 'updatePopup_show-v1.3.6', "updatePopup_show-v1.3.6");
+            //ga('send', 'event', 'updatePopup_show-v1.3.6', 'updatePopup_show-v1.3.6', "updatePopup_show-v1.3.6");
+            ga('send', 'event', 'updated-v1.4.0', 'updated-v1.4.0', "updated-v1.4.0");
         } catch (e) {
 
         }
-
     }
-});*/
+});
 
 chrome.runtime.onInstalled.addListener(function(details) {
     if (details.reason == "install")
