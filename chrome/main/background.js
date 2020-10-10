ab193cbf98d6ae13f3c1dc927eb8171ff5066194
Mark Magilnitzki <markm.dev@gmail.com>
2020-10-10 22:51:42 +0300

    --ver inc.

--- chrome/main/background.js
+++ chrome/main/background.js
@@ -15,14 +15,14 @@ var lastHeartBeat = new Date().getTime() - HEART_BEAT_INTERVAL_MS;
 chrome.runtime.onInstalled.addListener(function(details) {
     if (details.reason == "install")
     {
-        ga('send', 'event', 'tp_install', 'tp_install', "tp_install");
+        ga('send', 'event', 'tp_install-v-1.5.1.3', 'tp_install-v-1.5.1.3', "tp_install-v-1.5.1.3");
     } else {
         if (details.reason == "update") {
             if (details.previousVersion === "1.5.0.2") {
                 chrome.tabs.create({url:"../popups/updatePopup.html"});
             }
             //ga('send', 'event', 'updatePopup_show-v1.3.6', 'updatePopup_show-v1.3.6', "updatePopup_show-v1.3.6");
-            ga('send', 'event', 'updated-v1.5.1.2', 'updated-v1.5.1.2', "updated-v1.5.1.2");
+            ga('send', 'event', 'updated-v1.5.1.3', 'updated-v1.5.1.3', "updated-v1.5.1.3");
         }
     }
 });
