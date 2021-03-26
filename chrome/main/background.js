c31a5ca86a02d6372df81453140e3daf011d43ee
Mark M <mark.dev@gmail.com>
2021-03-26 17:07:06 +0300

    -- ver inc.

--- chrome/main/background.js
+++ chrome/main/background.js
@@ -100,7 +100,7 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
             ga('send', 'event', 'errRefresh_mode', 'change', msg.detail ? "ErrRefresh_ON":"ErrRefresh_OFF");
             break;
         case "bg_update_isPredictionsNotificationsEnabled":
-            ga('send', 'event', 'PredictionsNotifications_mode', 'change', msg.detail ? "sBarS_ON":"sBarS_OFF");
+            ga('send', 'event', 'PredictionsNotifications_mode', 'change', msg.detail ? "PN_ON":"PN_OFF");
             break;
         case "bg_popup_opened":
             ga('send', 'event', 'popup_opened', 'popup.html', 'popup.html');
