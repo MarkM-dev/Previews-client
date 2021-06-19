69dd1ebf2dd56c13c1d473f227b41520aac6403c
Mark M <mark.dev@gmail.com>
2021-06-19 04:01:37 +0300

    -- settings.

--- chrome/main/core.js
+++ chrome/main/core.js
@@ -1930,6 +1930,44 @@ function setAppVer(settingsContainer) {
     settingsContainer.querySelector('#tp_version').innerText = " - v" + chrome.runtime.getManifest().version;
 }
 
+function initDragForSettings(settingsContainer) {
+    dragElement(settingsContainer.querySelector('#TPBodyEl'));
+
+    function dragElement(elmnt) {
+        var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+        if (settingsContainer.querySelector('#tp_settings_title_container')) {
+            settingsContainer.querySelector('#tp_settings_title_container').onmousedown = dragMouseDown;
+        } else {
+            elmnt.onmousedown = dragMouseDown;
+        }
+
+        function dragMouseDown(e) {
+            e = e || window.event;
+            e.preventDefault();
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragElement;
+            document.onmousemove = elementDrag;
+        }
+
+        function elementDrag(e) {
+            e = e || window.event;
+            e.preventDefault();
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        }
+
+        function closeDragElement() {
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
+    }
+}
+
 function showSettingsMenu() {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', chrome.runtime.getURL('main/settings.html'), true);
@@ -1983,6 +2021,8 @@ function showSettingsMenu() {
         initSocialBtn(settingsContainer, 'contact', false);
 
         setAppVer(settingsContainer);
+
+        initDragForSettings(settingsContainer);
 
         document.body.appendChild(settingsContainer);
 
