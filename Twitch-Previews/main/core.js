be77adf4bc5bfb2ca20185d0c71af221729d841d
Mark M <mark.dev@gmail.com>
2021-08-28 20:04:48 +0300

    -- fscreenwithchat cancel return to mode on btn click.

--- Twitch-Previews/main/core.js
+++ Twitch-Previews/main/core.js
@@ -1937,6 +1937,7 @@ function setfScrnWithChatBtn() {
 
                 if (hasEnteredFScreenWithChat) {
                     toggle_fScrnWithChat(selected_mode);
+                    remove_fScrnWithChat_backToFullscreen_callback();
                     return;
                 }
                 if (menu_div.style.display === "none") {
