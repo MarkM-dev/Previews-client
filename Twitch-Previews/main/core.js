1f039a310de5907943db75238ae56f9ff57d1dfd
Mark M <mark.dev@gmail.com>
2021-09-07 00:37:34 +0300

    -- favorites dont show no live favorites text for experimental sidebar.

--- Twitch-Previews/main/core.js
+++ Twitch-Previews/main/core.js
@@ -1068,6 +1068,8 @@ function appendFavoritesBtn() {
 function setSidebarFavorites() {
     _browser.storage.local.get('favorites_arr',function (res) {
 
+        let isExperimentalSidebar = !!document.querySelector('.side-nav--hover-exp');
+
         let followed_channels_section = document.querySelector('.side-nav-section');
         if (followed_channels_section) {
             let favorites_section = followed_channels_section.cloneNode(true);
@@ -1081,8 +1083,11 @@ function setSidebarFavorites() {
                 if (title_figure) {
                     title_figure.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="20px" width="20px" xmlns="http://www.w3.org/2000/svg"><path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 0 0 .6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0 0 46.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3zM664.8 561.6l36.1 210.3L512 672.7 323.1 772l36.1-210.3-152.8-149L417.6 382 512 190.7 606.4 382l211.2 30.7-152.8 148.9z"></path></svg>';
                     title_figure.title = 'Favorite Channels';
+                    if(isExperimentalSidebar) {
+                        section_title.querySelector('h5').innerText = 'FAVORITE CHANNELS';
+                    }
                 } else {
-                    section_title.firstChild.innerText = 'FAVORITE CHANNELS';
+                    section_title.querySelector('h5').innerText = 'FAVORITE CHANNELS';
                 }
 
                 if (favorites_section.querySelector('.side-nav-show-more-toggle__button')) {
@@ -1105,13 +1110,14 @@ function setSidebarFavorites() {
                     }
                 }
 
-
-                if (!favorites_section.children[1].firstChild && !isNavBarCollapsed) {
-                    let div = document.createElement('div');
-                    div.innerText = 'No live favorites';
-                    div.style.padding = '0px 10px 5px 10px';
-                    div.style.color = 'grey';
-                    favorites_section.children[1].appendChild(div);
+                if(!isExperimentalSidebar) {
+                    if (!favorites_section.children[1].firstChild && !isNavBarCollapsed) {
+                        let div = document.createElement('div');
+                        div.innerText = 'No live favorites';
+                        div.style.padding = '0px 10px 5px 10px';
+                        div.style.color = 'grey';
+                        favorites_section.children[1].appendChild(div);
+                    }
                 }
 
 
