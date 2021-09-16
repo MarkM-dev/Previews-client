7de0412f5236b9d0858f7eecd297ad6f14992a84
Mark M <mark.dev@gmail.com>
2021-09-16 18:43:45 +0300

    -- screenshot btn.

--- Twitch-Previews/main/core.js
+++ Twitch-Previews/main/core.js
@@ -2180,7 +2180,7 @@ function appendScreenShotBtn() {
             btn_container.style.height = (ttv_fullscreen_btn_size.height || "30") + "px";
             btn_container.style.zIndex = "1";
 
-            btn_container.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="100%" width="70%" xmlns="http://www.w3.org/2000/svg"><g>' +
+            btn_container.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="100%" width="63%" xmlns="http://www.w3.org/2000/svg"><g>' +
                     '<path fill="none" d="M0 0h24v24H0z"></path>' +
                     '<path d="M9.827 21.763L14.31 14l3.532 6.117A9.955 9.955 0 0 1 12 22c-.746 0-1.473-.082-2.173-.237zM7.89 21.12A10.028 10.028 0 0 1 2.458 15h8.965L7.89 21.119zM2.05 13a9.964 9.964 ' +
                     '0 0 1 2.583-7.761L9.112 13H2.05zm4.109-9.117A9.955 9.955 0 0 1 12 2c.746 0 1.473.082 2.173.237L9.69 10 6.159 3.883zM16.11 2.88A10.028 10.028 0 0 1 21.542 9h-8.965l3.533-6.119zM21.95 ' +
@@ -2201,9 +2201,8 @@ function appendScreenShotBtn() {
                         createMultiStreamBox(getCurrentStreamerName(), true, false, false, URL.createObjectURL(blob_res));
                     })
                 });
-
             }
-            document.querySelector('.player-controls__right-control-group').firstChild.before(btn_container);
+            document.querySelector('.player-controls__right-control-group').children[2].before(btn_container);
         }
     } catch (e) {
 
