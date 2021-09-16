b357125de900caecb7e026eac5833fca571c57d5
Mark M <mark.dev@gmail.com>
2021-09-16 18:33:18 +0300

    -- screenshot btn icon.

--- Twitch-Previews/main/core.js
+++ Twitch-Previews/main/core.js
@@ -2180,9 +2180,12 @@ function appendScreenShotBtn() {
             btn_container.style.height = (ttv_fullscreen_btn_size.height || "30") + "px";
             btn_container.style.zIndex = "1";
 
-            btn_container.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 20 20" height="100%" width="70%" xmlns="http://www.w3.org/2000/svg">' +
-                '<path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 ' +
-                '1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>';
+            btn_container.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="100%" width="70%" xmlns="http://www.w3.org/2000/svg"><g>' +
+                    '<path fill="none" d="M0 0h24v24H0z"></path>' +
+                    '<path d="M9.827 21.763L14.31 14l3.532 6.117A9.955 9.955 0 0 1 12 22c-.746 0-1.473-.082-2.173-.237zM7.89 21.12A10.028 10.028 0 0 1 2.458 15h8.965L7.89 21.119zM2.05 13a9.964 9.964 ' +
+                    '0 0 1 2.583-7.761L9.112 13H2.05zm4.109-9.117A9.955 9.955 0 0 1 12 2c.746 0 1.473.082 2.173.237L9.69 10 6.159 3.883zM16.11 2.88A10.028 10.028 0 0 1 21.542 9h-8.965l3.533-6.119zM21.95 ' +
+                    '11a9.964 9.964 0 0 1-2.583 7.761L14.888 11h7.064z"></path></g>' +
+                '</svg>';
 
             btn_container.onclick = function (){
                 let video = document.querySelector('video');
