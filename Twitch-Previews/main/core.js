d240b59d3f6fc373e85168c1b070ff87e4c5a9a3
Mark M <mark.dev@gmail.com>
2021-09-18 02:49:55 +0300

    --seek.

--- Twitch-Previews/main/core.js
+++ Twitch-Previews/main/core.js
@@ -2166,19 +2166,34 @@ function setfScrnWithChatBtn() {
     }
 }
 
-function addSeekOverlay(left) {
+function addSeekOverlay(left, isEnd) {
     let container = document.createElement('div');
     container.classList.add('tp_seek_indication');
     if (left) {
-        container.innerHTML = "<div style='margin-left: 10%;' >" +
-            "<svg stroke=\"currentColor\" fill=\"currentColor\" stroke-width=\"0\" viewBox=\"0 0 24 24\" height=\"50px\" width=\"50px\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M12,2C6.486,2,2,6.486,2,12s4.486,10,10,10c5.514,0,10-4.486,10-10S17.514,2,12,2z M12,20c-4.411,0-8-3.589-8-8 s3.589-8,8-8s8,3.589,8,8S16.411,20,12,20z\"></path><path d=\"M11 16L11 8 6 12zM17 16L17 8 12 12z\"></path></svg>" +
-            "<div>5 seconds</div>" +
-            "</div>"
+        if(isEnd) {
+            container.innerHTML = "<div style='margin-left: 10%;' >" +
+                "<svg stroke=\"currentColor\" fill=\"none\" stroke-width=\"0\" viewBox=\"0 0 24 24\" height=\"50px\" width=\"50px\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M2 7H5V17H2V7Z\" fill=\"currentColor\"></path><path d=\"M6 12L13.0023 7.00003V17L6 12Z\" fill=\"currentColor\"></path><path d=\"M21.0023 7.00003L14 12L21.0023 17V7.00003Z\" fill=\"currentColor\"></path></svg>" +
+                "<div>End</div>" +
+                "</div>"
+        } else {
+            container.innerHTML = "<div style='margin-left: 10%;' >" +
+                "<svg stroke=\"currentColor\" fill=\"currentColor\" stroke-width=\"0\" viewBox=\"0 0 24 24\" height=\"50px\" width=\"50px\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M12,2C6.486,2,2,6.486,2,12s4.486,10,10,10c5.514,0,10-4.486,10-10S17.514,2,12,2z M12,20c-4.411,0-8-3.589-8-8 s3.589-8,8-8s8,3.589,8,8S16.411,20,12,20z\"></path><path d=\"M11 16L11 8 6 12zM17 16L17 8 12 12z\"></path></svg>" +
+                "<div>5 seconds</div>" +
+                "</div>"
+        }
+
     } else {
-        container.innerHTML = "<div style='margin-right: 10%;' >" +
-            "<svg stroke=\"currentColor\" fill=\"currentColor\" stroke-width=\"0\" viewBox=\"0 0 24 24\" height=\"50px\" width=\"50px\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M12,2C6.486,2,2,6.486,2,12s4.486,10,10,10s10-4.486,10-10S17.514,2,12,2z M12,20c-4.411,0-8-3.589-8-8s3.589-8,8-8 s8,3.589,8,8S16.411,20,12,20z\"></path><path d=\"M13 16L18 12 13 8zM7 16L12 12 7 8z\"></path></svg>" +
-            "<div>5 seconds</div>" +
-            "</div>"
+        if(isEnd) {
+            container.innerHTML = "<div style='margin-right: 10%;' >" +
+                "<svg stroke=\"currentColor\" fill=\"none\" stroke-width=\"0\" viewBox=\"0 0 24 24\" height=\"50px\" width=\"50px\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M21.0023 17H18.0023V7H21.0023V17Z\" fill=\"currentColor\"></path><path d=\"M17.0023 12L10 17V7L17.0023 12Z\" fill=\"currentColor\"></path><path d=\"M2 17L9.00232 12L2 7V17Z\" fill=\"currentColor\"></path></svg>" +
+                "<div>End</div>" +
+                "</div>"
+        } else {
+            container.innerHTML = "<div style='margin-right: 10%;' >" +
+                "<svg stroke=\"currentColor\" fill=\"currentColor\" stroke-width=\"0\" viewBox=\"0 0 24 24\" height=\"50px\" width=\"50px\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M12,2C6.486,2,2,6.486,2,12s4.486,10,10,10s10-4.486,10-10S17.514,2,12,2z M12,20c-4.411,0-8-3.589-8-8s3.589-8,8-8 s8,3.589,8,8S16.411,20,12,20z\"></path><path d=\"M13 16L18 12 13 8zM7 16L12 12 7 8z\"></path></svg>" +
+                "<div>5 seconds</div>" +
+                "</div>"
+        }
     }
 
     document.querySelector('.video-player__container').appendChild(container);
@@ -2195,18 +2210,22 @@ function seekVideo(left) {
     if (video.buffered.length === 0) {
         return;
     }
-    addSeekOverlay(left);
+
     if (left) {
         if (video.currentTime - 5 < video.buffered.start(0)) {
             video.currentTime = video.buffered.start(0) + 1.2;
+            addSeekOverlay(left, true);
         } else {
             video.currentTime-= 5;
+            addSeekOverlay(left, false);
         }
     } else {
         if (video.currentTime + 5 >= video.buffered.end(video.buffered.length - 1)) {
             video.currentTime = video.buffered.end(video.buffered.length - 1) - 1.2;
+            addSeekOverlay(left, true);
         } else {
             video.currentTime+= 5;
+            addSeekOverlay(left, false);
         }
     }
 }
