a9822022de0eaee7c02a425d7bb87b3f677040ca
Mark M <mark.dev@gmail.com>
2021-09-18 23:52:14 +0300

    -- cast overlay.

--- Twitch-Previews/main/core.js
+++ Twitch-Previews/main/core.js
@@ -3619,11 +3619,7 @@ function check_cast_start() {
                     setPvqc();
                     appendCastWorker();
                     setTimeout(()=>{
-                        overlay.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="100px" width="100px" style="position: absolute;margin-top: -250px;margin-right: -400px;" xmlns="http://www.w3.org/2000/svg">' +
-                                '<path fill-rule="evenodd" d="M6.5 4a.5.5 0 01.5-.5h5a.5.5 0 01.5.5v5a.5.5 0 01-1 0V4.5H7a.5.5 0 01-.5-.5z" clip-rule="evenodd"></path>' +
-                                '<path fill-rule="evenodd" d="M12.354 3.646a.5.5 0 010 .708l-9 9a.5.5 0 01-.708-.708l9-9a.5.5 0 01.708 0z" clip-rule="evenodd"></path>' +
-                            '</svg>' +
-                            'Select Your<br>Cast Device';
+                        overlay.innerText = 'Select Your\nCast Device';
                         document.querySelector('.tw-chromecast-button__icon').closest('button').click();
                     }, 1000);
                     clearInterval(interval);
