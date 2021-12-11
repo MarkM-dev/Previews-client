88786fd77b3074a0a97ad69550e12983f69ccdc2
Mark M <mark.dev@gmail.com>
2021-12-12 00:33:08 +0200

    -- ver inc.

--- Twitch-Previews/main/background.js
+++ Twitch-Previews/main/background.js
@@ -146,10 +146,11 @@ _browser.runtime.onInstalled.addListener(function(details) {
         _browser.storage.local.set({'tpInstallTime': new Date().getTime()}, function() {});
     } else {
         if (details.reason === "update") {
-            if (details.previousVersion !== "2.5" && details.previousVersion !== "2.6") {
-                _browser.storage.local.set({'shouldShowUpdatePopup': true}, function() {});
-                _browser.storage.local.set({'shouldShowNewFeatureSettingsSpan': true}, function() {});
-            }
+            _browser.storage.local.set({'shouldShowUpdatePopup': true}, function() {});
+            _browser.storage.local.set({'shouldShowNewFeatureSettingsSpan': true}, function() {});
+
+            _browser.storage.local.set({'shouldShowDelayedRateToast': false}, function() {});
+
             _browser.storage.local.get('tpInstallTime', function(result) {
                 if (!result.tpInstallTime) {
                     _browser.storage.local.set({'shouldShowDelayedRateToast': true}, function() {});
