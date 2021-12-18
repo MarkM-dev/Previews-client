5a253c34cc387f107baacc24aecac5f9d1d8cfd2
Mark M <mark.dev@gmail.com>
2021-12-19 01:59:07 +0200

    -- langs - settings.

--- Twitch-Previews/main/tp_i18n.js
+++ Twitch-Previews/main/tp_i18n.js
@@ -1,4 +1,11 @@
 export let i18n = {
+    app_name: {
+        'en': 'Twitch Previews',
+        'es': 'Twitch Previews',
+        'de': 'Twitch Previews',
+        'fr': 'Twitch Previews',
+        'pt_BR': 'Twitch Previews'
+    },
     availableLangs: {
         'en': 'English',
         'es': 'español',
@@ -6,15 +13,358 @@ export let i18n = {
         'fr': 'Français',
         'pt_BR': 'português (Brasil)'
     },
-    settings_feature_name: {
-        'en': 'English',
+    settings_refresh_msg: {
+        'en': 'Refresh your Twitch tabs for changes to take effect',
         'es': '',
         'de': '',
         'fr': '',
         'pt_BR': ''
     },
-    settings_feature_name1: {
-        'en': 'English',
+    settings_feature_subtext_shows_in_player_controls: {
+        'en': '(shows in player controls)',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_section_previews: {
+        'en': 'Previews',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isDirpEnabled: {
+        'en': 'Directory Previews',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isSelfPreviewEnabled: {
+        'en': 'Streaming - See Your Own Live Stream Thumbnail',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_section_misc: {
+        'en': 'Misc',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isChannelPointsClickerEnabled: {
+        'en': 'Auto Channel Points Clicker',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isMultiStreamEnabled: {
+        'en': 'Multi-Stream & Multi-Chat',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isAdvancedVideoEmbedsEnabled: {
+        'en': 'Advanced Video Embeds',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isClipDownloaderEnabled: {
+        'en': 'Clip Download Button',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isMuteAutoPlayersEnabled: {
+        'en': 'Mute Auto-Playing Videos In Various Pages',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_subtext_isMuteAutoPlayersEnabled: {
+        'en': '(homepage, offline stream page, etc..)',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_section_sidebar: {
+        'en': 'Sidebar',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isSidebarFavoritesEnabled: {
+        'en': 'Sidebar Favorite Channels',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_subtext_isSidebarFavoritesEnabled: {
+        'en': '(shows at the top of the sidebar)',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isYTsidebarEnabled: {
+        'en': 'Sidebar YouTube Channels',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_subtext_isYTsidebarEnabled: {
+        'en': '(shows at the top of the sidebar)',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isSidebarExtendEnabled: {
+        'en': 'Auto Extend Sidebar (show more)',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_subtext_isSidebarExtendEnabled: {
+        'en': '(when sidebar is open)',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isSidebarSearchEnabled: {
+        'en': 'Sidebar Search Button',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isSidebarHideSectionsEnabled: {
+        'en': 'Hide All Sidebar Sections Except The Followed Channels',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_section_player: {
+        'en': 'Player',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isErrRefreshEnabled: {
+        'en': 'Auto Refresh On Twitch Player Error',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_subtext_isErrRefreshEnabled: {
+        'en': '(#1000, #2000, #3000, #4000)',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isPvqcEnabled: {
+        'en': 'Prevent Auto Video Quality Change When In Background',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isfScrnWithChatEnabled: {
+        'en': 'Full Screen + Chat Button',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isPipEnabled: {
+        'en': 'Picture In Picture Button',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isFastForwardEnabled: {
+        'en': 'Fast-Forward Button',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_subtext_isFastForwardEnabled: {
+        'en': '(shows in player controls, useful if stream is delayed)',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isSeekEnabled: {
+        'en': 'Seek Streams Using Keyboard Arrow Keys',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isScreenshotEnabled: {
+        'en': 'Screenshot Stream Button',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isFlashBangDefenderEnabled: {
+        'en': 'FlashBang Defender Button',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isCastEnabled: {
+        'en': 'Chrome-Cast -> Close Tab Button',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_section_chat: {
+        'en': 'Chat',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isClearChatEnabled: {
+        'en': 'Clear Chat Button',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_subtext_isClearChatEnabled: {
+        'en': '(shows under the chat)',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_section_predictions: {
+        'en': 'Predictions',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isPredictionsNotificationsEnabled: {
+        'en': 'Predictions Notifications',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_subtext_isPredictionsNotificationsEnabled: {
+        'en': '(works on twitch tabs in the browser)',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isPredictionsSniperEnabled: {
+        'en': 'Predictions Sniper',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_subtext_isPredictionsSniperEnabled: {
+        'en': 'Default settings:',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_section_settings: {
+        'en': 'Settings',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_import_export: {
+        'en': 'Import/Export Settings',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_subtext_import_export: {
+        'en': '(expand for settings selection)',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_isSidebarPreviewsEnabled: {
+        'en': 'Sidebar Previews',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_preview_image_preview: {
+        'en': 'Image Preview',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_preview_video_preview: {
+        'en': 'Video Preview',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_feature_preview_size: {
+        'en': 'Preview Size:',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_donate_above_text: {
+        'en': 'If you like this extension, consider\ncontributing :)',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_rate: {
+        'en': 'Rate',
+        'es': '',
+        'de': '',
+        'fr': '',
+        'pt_BR': ''
+    },
+    settings_share: {
+        'en': 'Share',
         'es': '',
         'de': '',
         'fr': '',
