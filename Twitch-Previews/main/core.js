1e47d945f5d08e22f86ca8478d5df8df183202e3
Mark M <mark.dev@gmail.com>
2022-01-25 10:18:35 +0200

    -- update toast.

--- Twitch-Previews/main/core.js
+++ Twitch-Previews/main/core.js
@@ -4478,11 +4478,21 @@
             "                <div style=\"display: inline-block;padding: 5px;cursor: pointer;font-weight: bold;\" id='tp_updateToast_rate_btn' >" + _i18n('update_toast_rate') + "</div>\n" +
             "               | <div style=\"display: inline-block;padding: 5px;cursor: pointer;font-weight: bold;\" id='tp_updateToast_share_btn' >" + _i18n('update_toast_share') + "</div>\n" +
             "               | <div style=\"display: inline-block;padding: 5px;cursor: pointer;font-weight: bold;\" id='tp_updateToast_settings_btn' >" + _i18n('update_toast_settings') + "</div>\n" +
-            "                <div id=\"tp_updateToast_donate_btn\" >\n" +
-            "                                <img id=\"tp_updateToast_donate_btn_img\" src=\"" + getRuntimeUrl('images/donate_heart.png') + "\" width=\"22\" height=\"20\" >\n" +
-            "                                <span id=\"tp_updateToast_donate_btn_buffer_span\" ></span>\n" +
-            "                                <span id=\"tp_updateToast_donate_btn_text_span\" >" + _i18n('donate_btn_text') + "</span>\n" +
-            "                            </div>\n" +
+            "                <div id=\"tp_updateToast_donate_btn\" class=\"tp_updateToast_donate_btn\" >\n" +
+    "                                <img id=\"tp_updateToast_donate_btn_img\" src=\"" + getRuntimeUrl('images/donate_heart.png') + "\" width=\"22\" height=\"20\" >\n" +
+    "                                <span id=\"tp_updateToast_donate_btn_buffer_span\" ></span>\n" +
+    "                                <span id=\"tp_updateToast_donate_btn_text_span\" >" + _i18n('donate_btn_text') + "</span>\n" +
+"                            </div>\n" +
+    "                       <div id=\"tp_updateToast_donate_bitcoin_btn\" class=\"tp_updateToast_donate_btn\" style=\"width: 14%;\" >\n" +
+    "                                <svg stroke=\"currentColor\" fill=\"currentColor\" stroke-width=\"0\" viewBox=\"0 0 512 512\" height=\"20px\" width=\"20px\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+    "                                    <path d=\"M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zm-141.651-35.33c4.937-32.999-20.191-50.739-54.55-62.573l11.146-44.702-27.213-6.781-10.851\n" +
+    "                                    43.524c-7.154-1.783-14.502-3.464-21.803-5.13l10.929-43.81-27.198-6.781-11.153 44.686c-5.922-1.349-11.735-2.682-17.377-4.084l.031-.14-37.53-9.37-7.239 29.062s20.191 4.627 19.765 4.913c11.022\n" +
+    "                                    2.751 13.014 10.044 12.68 15.825l-12.696 50.925c.76.194 1.744.473 2.829.907-.907-.225-1.876-.473-2.876-.713l-17.796 71.338c-1.349 3.348-4.767 8.37-12.471 6.464.271.395-19.78-4.937-19.78-4.937l-13.51\n" +
+    "                                    31.147 35.414 8.827c6.588 1.651 13.045 3.379 19.4 5.006l-11.262 45.213 27.182 6.781 11.153-44.733a1038.209 1038.209 0 0 0 21.687 5.627l-11.115 44.523 27.213 6.781 11.262-45.128c46.404 8.781 81.299 5.239\n" +
+    "                                    95.986-36.727 11.836-33.79-.589-53.281-25.004-65.991 17.78-4.098 31.174-15.792 34.747-39.949zm-62.177 87.179c-8.41 33.79-65.308 15.523-83.755 10.943l14.944-59.899c18.446 4.603 77.6 13.717 68.811 48.956zm8.417-87.667c-7.673\n" +
+    "                                    30.736-55.031 15.12-70.393 11.292l13.548-54.327c15.363 3.828 64.836 10.973 56.845 43.035z\"></path>\n" +
+    "                                </svg>\n" +
+    "                            </div>" +
             "            </div>\n" +
             "            <div style=\"margin-top: 5px;padding: 5px;cursor: pointer;font-size: 12px;text-align: center;font-weight: bold;\" id='tp_updateToast_dismiss_btn' >" + (isDelayedRateToast ? _i18n('delayed_rate_toast_close'):_i18n('update_toast_close')) + "</div>\n" +
             "        </div>";
@@ -4503,6 +4513,11 @@
         updateToast.querySelector('#tp_updateToast_donate_btn').onclick = function () {
             sendMessageToBG({action: "bg_show_donate", detail: ""});
             sendMessageToBG({action: toastType + "_donate_btn_click", detail: ""});
+        };
+
+        updateToast.querySelector('#tp_updateToast_donate_bitcoin_btn').onclick = function () {
+            sendMessageToBG({action: "bg_show_donate_bitcoin", detail: ""});
+            sendMessageToBG({action: toastType + "_donate_bitcoin_btn_click", detail: ""});
         };
 
         updateToast.querySelector('#tp_updateToast_dismiss_btn').onclick = function () {
