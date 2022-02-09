4600599b654c74ac2b79b66e023b25270f4e7125
Mark M <mark.dev@gmail.com>
2022-02-09 21:17:40 +0200

    -- icons for experimental sidebar.

--- Twitch-Previews/main/core.js
+++ Twitch-Previews/main/core.js
@@ -1121,9 +1121,22 @@
                     let title_figure = section_title.querySelector('figure');
                     if (title_figure) {
                         if(isExperimentalSidebar) {
-                            title_figure.innerHTML = '';
-                            title_figure.style.width = '20px';
-                            title_figure.style.height = '20px';
+                            if (options.isSidebarFavoritesEnabled) {
+                                title_figure.title = _i18n('sidebar_yt_channels_title');
+                                title_figure.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="21px" width="21px" xmlns="http://www.w3.org/2000/svg">' +
+                                    '<path d="M960 509.2c0-2.2 0-4.7-.1-7.6-.1-8.1-.3-17.2-.5-26.9-.8-27.9-2.2-55.7-4.4-81.9-3-36.1-7.4-66.2-13.4-88.8a139.52 139.52 0 0 0-98.3-98.5c-28.3-7.6-83.7-12.3-161.7-15.2-37.1-1.4-76.8-2.3-116.5-2.8-13.9-.2-26.8-.3-38.4-.4h-29.4c-11.6.1-24.5.2-38.4.4-39.7.5-79.4 ' +
+                                    '1.4-116.5 2.8-78 3-133.5 7.7-161.7 15.2A139.35 139.35 0 0 0 82.4 304C76.3 326.6 72 356.7 69 392.8c-2.2 26.2-3.6 54-4.4 81.9-.3 9.7-.4 18.8-.5 26.9 0 2.9-.1 5.4-.1 7.6v5.6c0 ' +
+                                    '2.2 0 4.7.1 7.6.1 8.1.3 17.2.5 26.9.8 27.9 2.2 55.7 4.4 81.9 3 36.1 7.4 66.2 13.4 88.8 12.8 47.9 50.4 85.7 98.3 98.5 28.2 7.6 83.7 12.3 161.7 15.2 37.1 1.4 76.8 2.3 116.5 2.8 13.9.2 ' +
+                                    '26.8.3 38.4.4h29.4c11.6-.1 24.5-.2 38.4-.4 39.7-.5 79.4-1.4 116.5-2.8 78-3 133.5-7.7 161.7-15.2 47.9-12.8 85.5-50.5 98.3-98.5 6.1-22.6 10.4-52.7 13.4-88.8 2.2-26.2 3.6-54 4.4-81.9.3-9.7.4-18.8.5-26.9 0-2.9.1-5.4.1-7.6v-5.6zm-72 5.2c0 2.1 ' +
+                                    '0 4.4-.1 7.1-.1 7.8-.3 16.4-.5 25.7-.7 26.6-2.1 53.2-4.2 77.9-2.7 32.2-6.5 58.6-11.2 76.3-6.2 23.1-24.4 41.4-47.4 47.5-21 5.6-73.9 10.1-145.8 12.8-36.4 1.4-75.6 2.3-114.7 2.8-13.7.2-26.4.3-37.8.3h-28.6l-37.8-.3c-39.1-.5-78.2-1.4-114.7-2.8-71.9-2.8-124.9-7.2-145.8-12.8-23-6.' +
+                                    '2-41.2-24.4-47.4-47.5-4.7-17.7-8.5-44.1-11.2-76.3-2.1-24.7-3.4-51.3-4.2-77.9-.3-9.3-.4-18-.5-25.7 0-2.7-.1-5.1-.1-7.1v-4.8c0-2.1 0-4.4.1-7.1.1-7.8.3-16.4.5-25.7.7-26.6 2.1-53.2 4.2-77.9 2.7-32.2 6.5-58.6 11.2-76.3 6.2-23.1 24.4-41.4 47.4-47.5 21-5.6 73.9-10.1 145.8-12.8 36.4-1.4 75.6-2.3 ' +
+                                    '114.7-2.8 13.7-.2 26.4-.3 37.8-.3h28.6l37.8.3c39.1.5 78.2 1.4 114.7 2.8 71.9 2.8 124.9 7.2 145.8 12.8 23 6.2 41.2 24.4 47.4 47.5 4.7 17.7 8.5 44.1 11.2 76.3 2.1 24.7 3.4 51.3 4.2 77.9.3 9.3.4 18 .5 25.7 0 2.7.1 5.1.1 7.1v4.8zM423 646l232-135-232-133z"></path>' +
+                                    '</svg>';
+                            } else {
+                                title_figure.innerHTML = '';
+                                title_figure.style.width = '20px';
+                                title_figure.style.height = '20px';
+                            }
                             section_title.querySelector('h5').innerText = _i18n('sidebar_yt_channels_section_title');
                         } else {
                             title_figure.title = _i18n('sidebar_yt_channels_title');
@@ -1236,9 +1249,14 @@
                     let title_figure = section_title.querySelector('figure');
                     if (title_figure) {
                         if(isExperimentalSidebar) {
-                            title_figure.innerHTML = '';
-                            title_figure.style.width = '20px';
-                            title_figure.style.height = '20px';
+                            if (options.isSidebarFavoritesEnabled || options.isYTsidebarEnabled) {
+                                title_figure.title = _i18n('sidebar_fb_channels_title');
+                                title_figure.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="21px" width="21px" xmlns="http://www.w3.org/2000/svg"><g><path fill="none" d="M0 0h24v24H0z"></path><path d="M13 19.938A8.001 8.001 0 0 0 12 4a8 8 0 0 0-1 15.938V14H9v-2h2v-1.654c0-1.337.14-1.822.4-2.311A2.726 2.726 0 0 1 12.536 6.9c.382-.205.857-.328 1.687-.381.329-.021.755.005 1.278.08v1.9H15c-.917 0-1.296.043-1.522.164a.727.727 0 0 0-.314.314c-.12.226-.164.45-.164 1.368V12h2.5l-.5 2h-2v5.938zM12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10z"></path></g></svg>';
+                            } else {
+                                title_figure.innerHTML = '';
+                                title_figure.style.width = '20px';
+                                title_figure.style.height = '20px';
+                            }
                             section_title.querySelector('h5').innerText = _i18n('sidebar_fb_channels_section_title');
                         } else {
                             title_figure.title = _i18n('sidebar_fb_channels_title');
