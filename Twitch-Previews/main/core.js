9e949cbf76bb5cdb8a53f03a8b142a41d101643f
Mark M <mark.dev@gmail.com>
2022-03-27 17:35:13 +0300

    -- update toast.

--- Twitch-Previews/main/core.js
+++ Twitch-Previews/main/core.js
@@ -4884,13 +4884,26 @@
         //let ffclass = isFirefox ? 'class="tp_display_none"':'';
         //let cclass = isFirefox ? '':'class="tp_display_none"';
         return "   <div style=\"font-weight: bold;font-size: 15px;color: white;\" >" + _i18n('update_toast_updated_title') + "</div>"
+/*
             +  "       <div style=\"font-size: 14px;font-weight: bold;margin-top: 10px;color: white;\" >New Features!</div>"
-            +  "       <div style=\"font-size: 14px;color: white;margin-top: 20px;\" ><strong style='color: #2cff95;' >- Sidebar Drag & Drop Video Embeds!</strong>"
-            +  "             <span ><br><span style=\"font-size: 12px;color: whitesmoke;font-weight: bold;\" >- Drag & Drop streamers from the sidebar onto the page to add as a video embed.</span>"
-            +  "             <br><span ><br><span style=\"font-size: 12px;color: whitesmoke;\" >- Very convenient when the sidebar is closed, no need to scroll and open the sidebar to click the squares video embed button.</span>"
-            +  "             <span ><br><span style=\"font-size: 12px;color: whitesmoke;\" id='tp_updateToast_discord_btn' >- Discord.</span>"
-            +  "             <span ><br><span style=\"font-size: 12px;color: whitesmoke;\" >Pro-Tip: Enable the 'Advanced video embeds' feature for a better video embed experience.</span>"
+*/
+            +  "       <div style=\"font-size: 14px;color: white;margin-top: 20px;\" ><strong style='color: #2cff95;' >- Discord Server!</strong>"
+            +  "             <span ><br><span style=\"font-size: 12px;color: whitesmoke;font-weight: bold;\" >- We opened a Discord server!</span>"
+            +  "             <br><span style=\"font-size: 12px;color: whitesmoke;\" >- It's pretty basic right now, let us know how we can improve it! :)</span>"
+            +  "             <br><span id='tp_updateToast_discord_btn' >" +
+            "<svg stroke=\"currentColor\" fill=\"currentColor\" stroke-width=\"0\" role=\"img\" viewBox=\"0 0 24 24\" height=\"1em\" width=\"1em\" " +
+            "xmlns=\"http://www.w3.org/2000/svg\"><title></title><path d=\"M20.317 4.3698a19.7913 19.7913 0 00-4.8851-1.5152.0741.0741 0 00-.0785.0371c-.211.3753-.4447.8648-.6083 " +
+            "1.2495-1.8447-.2762-3.68-.2762-5.4868 0-.1636-.3933-.4058-.8742-.6177-1.2495a.077.077 0 00-.0785-.037 19.7363 19.7363 0 00-4.8852 1.515.0699.0699 0 00-.0321.0277C.5334 " +
+            "9.0458-.319 13.5799.0992 18.0578a.0824.0824 0 00.0312.0561c2.0528 1.5076 4.0413 2.4228 5.9929 3.0294a.0777.0777 0 00.0842-.0276c.4616-.6304.8731-1.2952 1.226-1.9942a.076.076 " +
+            "0 00-.0416-.1057c-.6528-.2476-1.2743-.5495-1.8722-.8923a.077.077 0 01-.0076-.1277c.1258-.0943.2517-.1923.3718-.2914a.0743.0743 0 01.0776-.0105c3.9278 1.7933 8.18 1.7933 12.0614 " +
+            "0a.0739.0739 0 01.0785.0095c.1202.099.246.1981.3728.2924a.077.077 0 01-.0066.1276 12.2986 12.2986 0 01-1.873.8914.0766.0766 0 00-.0407.1067c.3604.698.7719 1.3628 1.225 " +
+            "1.9932a.076.076 0 00.0842.0286c1.961-.6067 3.9495-1.5219 6.0023-3.0294a.077.077 0 00.0313-.0552c.5004-5.177-.8382-9.6739-3.5485-13.6604a.061.061 0 " +
+            "00-.0312-.0286zM8.02 15.3312c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9555-2.4189 2.157-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.9555 2.4189-2.1569 " +
+            "2.4189zm7.9748 0c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9554-2.4189 2.1569-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.946 2.4189-2.1568 2.4189Z\"></path>" +
+            "</svg>&nbsp;Join Our Discord!</span>"
+/*
             +  "             <span ><br><br><span style=\"font-size: 12px;color: whitesmoke;\" ><img height='136' width='263' style='margin-top: 5px;' src='" + getRuntimeUrl('images/updatetoast_img.jpg') + "' ></span>"
+*/
             +  "       </div>"
             +  "    </br>"
     }
