7a34daf42763b05daf5cdb74cdf7d489f85b44c0
Mark M <mark.dev@gmail.com>
2022-05-21 00:48:21 +0300

    -- fix for sidebar channels.

--- Twitch-Previews/main/core.js
+++ Twitch-Previews/main/core.js
@@ -1209,7 +1209,8 @@
                             title_figure.style.width = '20px';
                             title_figure.style.height = '20px';
                         }
-                        section_title.querySelector('h5').innerText = _i18n('sidebar_yt_channels_section_title');
+                        let title_el_text_el = section_title.querySelector('h5') || section_title.querySelector('h2');
+                        title_el_text_el.innerText = _i18n('sidebar_yt_channels_section_title');
                     } else {
                         title_figure.title = _i18n('sidebar_yt_channels_title');
                         title_figure.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="21px" width="21px" xmlns="http://www.w3.org/2000/svg">' +
@@ -1223,7 +1224,8 @@
                             '</svg>';
                     }
                 } else {
-                    section_title.querySelector('h5').innerText = _i18n('sidebar_yt_channels_section_title');
+                    let title_el_text_el = section_title.querySelector('h5') || section_title.querySelector('h2');
+                    title_el_text_el.innerText = _i18n('sidebar_yt_channels_section_title');
                 }
 
                 if (yt_section.querySelector('.side-nav-show-more-toggle__button')) {
@@ -1336,13 +1338,15 @@
                             title_figure.style.width = '20px';
                             title_figure.style.height = '20px';
                         }
-                        section_title.querySelector('h5').innerText = _i18n('sidebar_fb_channels_section_title');
+                        let title_el_text_el = section_title.querySelector('h5') || section_title.querySelector('h2');
+                        title_el_text_el.innerText = _i18n('sidebar_yt_channels_section_title');
                     } else {
                         title_figure.title = _i18n('sidebar_fb_channels_title');
                         title_figure.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="21px" width="21px" xmlns="http://www.w3.org/2000/svg"><g><path fill="none" d="M0 0h24v24H0z"></path><path d="M13 19.938A8.001 8.001 0 0 0 12 4a8 8 0 0 0-1 15.938V14H9v-2h2v-1.654c0-1.337.14-1.822.4-2.311A2.726 2.726 0 0 1 12.536 6.9c.382-.205.857-.328 1.687-.381.329-.021.755.005 1.278.08v1.9H15c-.917 0-1.296.043-1.522.164a.727.727 0 0 0-.314.314c-.12.226-.164.45-.164 1.368V12h2.5l-.5 2h-2v5.938zM12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10z"></path></g></svg>';
                     }
                 } else {
-                    section_title.querySelector('h5').innerText = _i18n('sidebar_fb_channels_section_title');
+                    let title_el_text_el = section_title.querySelector('h5') || section_title.querySelector('h2');
+                    title_el_text_el.innerText = _i18n('sidebar_yt_channels_section_title');
                 }
 
                 if (fb_section.querySelector('.side-nav-show-more-toggle__button')) {
@@ -1596,7 +1600,8 @@
                             title_figure.innerHTML = '';
                             title_figure.style.width = '20px';
                             title_figure.style.height = '20px';
-                            section_title.querySelector('h5').innerText = _i18n('sidebar_favorite_channels_section_title');
+                            let title_el_text_el = section_title.querySelector('h5') || section_title.querySelector('h2');
+                            title_el_text_el.innerText = _i18n('sidebar_yt_channels_section_title');
                         } else {
                             title_figure.title = _i18n('sidebar_favorite_channels_title');
                             title_figure.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="20px" width="20px" xmlns="http://www.w3.org/2000/svg">' +
@@ -1606,7 +1611,8 @@
                                 '</svg>';
                         }
                     } else {
-                        section_title.querySelector('h5').innerText = _i18n('sidebar_favorite_channels_section_title');
+                        let title_el_text_el = section_title.querySelector('h5') || section_title.querySelector('h2');
+                        title_el_text_el.innerText = _i18n('sidebar_yt_channels_section_title');
                     }
 
                     if (favorites_section.querySelector('.side-nav-show-more-toggle__button')) {
