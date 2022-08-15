df078a615cc8d51290824cc65d57fbbd28701c6b
Mark M <mark.dev@gmail.com>
2022-08-15 05:55:02 +0300

    -- update toast.

--- Twitch-Previews/main/background.js
+++ Twitch-Previews/main/background.js
@@ -257,11 +257,9 @@ _browser.runtime.onInstalled.addListener(function(details) {
                 }
             });
 
-            /*if (details.previousVersion !== "3.9" && details.previousVersion !== "4.0" && details.previousVersion !== "4.1") {
-                _browser.storage.local.set({'shouldShowUpdatePopup': true}, function() {});
-                //_browser.storage.local.set({'shouldShowNewFeatureSettingsSpan': true}, function() {});
-                _browser.storage.local.set({'shouldShowDelayedRateToast': false}, function() {});
-            }*/
+            _browser.storage.local.set({'shouldShowUpdatePopup': true}, function() {});
+            _browser.storage.local.set({'shouldShowNewFeatureSettingsSpan': true}, function() {});
+            _browser.storage.local.set({'shouldShowDelayedRateToast': false}, function() {});
 
            /* if (navigator_lang === 'ko') {
                 _browser.storage.local.set({'shouldShowNewLangToast': true}, function() {});
