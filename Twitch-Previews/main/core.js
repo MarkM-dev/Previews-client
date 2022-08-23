45a7a45af42a9e065070880329c0cc58fb25118e
Mark M <mark.dev@gmail.com>
2022-08-23 17:42:38 +0300

    -- fix for favorites sidebar partial list icon.

--- Twitch-Previews/main/core.js
+++ Twitch-Previews/main/core.js
@@ -2114,7 +2114,7 @@
 
                             if (promise_result === 'FOLLOWED_LIST_IS_PARTIAL') {
                                 if (section_header_figure) {
-                                    section_header_figure.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="20px" width="20px" xmlns="http://www.w3.org/2000/svg">' +
+                                    section_header_figure.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="20px" width="100%" xmlns="http://www.w3.org/2000/svg">' +
                                         '<path d="M 908.1 353.1 l -253.9 -36.9 L 540.7 86.1 c -3.1 -6.3 -8.2 -11.4 -14.5 -14.5 c -15.8 -7.8 -35 -1.3 -42.9 14.5 L 369.8 316.2 l -253.9 36.9 c -7 1 -13.4 4.3 -18.3 ' +
                                         '9.3 a 32.05 32.05 0 0 0 0.6 45.3 l 183.7 179.1 l -43.4 252.9 a 31.95 31.95 0 0 0 46.4 33.7 L 512 754 l 227.1 119.4 c 6.2 3.3 13.4 4.4 20.3 3.2 c 17.4 -3 29.1 -19.5 26.1 -36.9 l ' +
                                         '-43.4 -252.9 l 183.7 -179.1 c 5 -4.9 8.3 -11.3 9.3 -18.3 c 2.7 -17.5 -9.5 -33.7 -27 -36.3 z M 664.8 561.6 l 36.1 210.3 L 512 672.7 L 323.1 772 l 36.1 -210.3 l -152.8 -149 L 417.6 382 ' +
