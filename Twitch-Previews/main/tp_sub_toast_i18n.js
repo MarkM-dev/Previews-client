727966f9b35d76729ddc976aa07252b55d4be915
Mark M <mark.dev@gmail.com>
2022-11-18 00:33:03 +0200

    -- sub toast.

--- Twitch-Previews/main/tp_sub_toast_i18n.js
+++ Twitch-Previews/main/tp_sub_toast_i18n.js
@@ -21,43 +21,43 @@ export const i18n = {
     },
     gifted_sub_ended_toast_body: {
         'en': 'Your %s gifted subscription has ended.\nSubscribe or redeem a code to continue your subscription.',
-        'es': '',
-        'de': '',
-        'fr': '',
-        'pt_BR': '',
-        'ko': '',
-        'ja': '',
-        'ru':''
+        'es': 'Su suscripción de regalo de %s ha finalizado.\nSuscríbete o canjea un código para continuar con tu suscripción.',
+        'de': 'Ihr geschenktes Abonnement von %s ist abgelaufen.\nAbonnieren Sie oder lösen Sie einen Code ein, um Ihr Abonnement fortzusetzen.',
+        'fr': 'Votre abonnement gratuit %s est terminé.\nAbonnez-vous ou utilisez un code pour continuer votre abonnement.',
+        'pt_BR': 'Sua assinatura de presente %s terminou.\nAssine ou resgate um código para continuar sua assinatura.',
+        'ko': '귀하의 %s 선물 구독이 종료되었습니다.\n구독을 계속하려면 코드를 구독하거나 교환하세요.',
+        'ja': '%s ギフト サブスクリプションは終了しました。\nサブスクリプションを継続するには、サブスクライブするか、コードを引き換えてください。',
+        'ru':'Ваша подарочная подписка %s закончилась.\nПодпишитесь или активируйте код, чтобы продолжить подписку.'
     },
     subscribe_toast_have_code: {
         'en': 'Redeem Code',
-        'es': '',
-        'de': '',
-        'fr': '',
-        'pt_BR': '',
-        'ko': '',
-        'ja': '',
-        'ru':''
+        'es': 'Canjear código',
+        'de': 'Code einlösen',
+        'fr': 'Utiliser le code',
+        'pt_BR': 'Resgatar código',
+        'ko': '코드를 사용',
+        'ja': 'コードを引き換える',
+        'ru':'Активировать код'
     },
     subscribe_toast_close: {
         'en': 'Not now',
-        'es': '',
-        'de': '',
-        'fr': '',
-        'pt_BR': '',
-        'ko': '',
-        'ja': '',
-        'ru':''
+        'es': 'no ahora',
+        'de': 'Nicht jetzt',
+        'fr': 'Pas maintenant',
+        'pt_BR': 'Agora não',
+        'ko': '지금은 아니다',
+        'ja': '今はやめろ',
+        'ru':'Не сейчас'
     },
     subscribe_toast_subscribe: {
         'en': 'Subscribe',
-        'es': '',
-        'de': '',
-        'fr': '',
-        'pt_BR': '',
-        'ko': '',
-        'ja': '',
-        'ru':''
+        'es': 'Suscribir',
+        'de': 'Abonnieren',
+        'fr': 'S\'abonner',
+        'pt_BR': 'Se inscrever',
+        'ko': '구독하다',
+        'ja': '申し込む',
+        'ru':'Подписывайся'
     },
     subscribe_toast_settings: {
         'en': 'Settings',
